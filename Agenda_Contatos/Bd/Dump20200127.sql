751ebc7704595a9aadf9f60e19743681fd1b72ec
Idyl Icaro dos Santos <45442467+idylicaro@users.noreply.github.com>
2020-01-27 13:13:38 -0300

    update bd file

--- Agenda_Contatos/Bd/Dump20200127.sql
+++ Agenda_Contatos/Bd/Dump20200127.sql
@@ -1,3 +1,5 @@
+CREATE DATABASE  IF NOT EXISTS `ageconbd` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `ageconbd`;
 -- MySQL dump 10.13  Distrib 8.0.19, for Win64 (x86_64)
 --
 -- Host: localhost    Database: ageconbd
@@ -47,4 +49,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2020-01-27 12:31:19
+-- Dump completed on 2020-01-27 13:13:11
